acfc01a8482d668d7d71d9ea317f4026ac5dc4eb
Simrat Grewal <simrats169169@gmail.com>
2022-09-04 21:00:24 -0700

    Fix checkstyle issues

--- src/main/java/com/comp2522/Main.java
+++ src/main/java/com/comp2522/Main.java
@@ -1,7 +1,10 @@
 package com.comp2522;
 
+/**
+ * The entry point of the program.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello gamers!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello gamers!");
+  }
 }
